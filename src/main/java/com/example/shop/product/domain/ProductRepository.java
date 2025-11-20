993dc80929b62d7ca758a29a07236b077e3853f4
cje206 <projec206@gmail.com>
2025-11-20 12:47:37 +0900

    Seller DDD

---
+++ src/main/java/com/example/shop/product/domain/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.example.shop.product.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepository {
+
+    Page<Product> findAll(Pageable pageable);
+
+    Product save(Product product);
+
+    Optional<Product> findById(UUID id);
+
+    void deleteById(UUID id);
+}
